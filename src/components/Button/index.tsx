3755dcc616fb4918d2eace4b8ac14703ac51a276
bonetou <h.boneto@outlook.com>
2023-03-19 17:11:13 -0300

    feat: create form

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 class Button extends React.Component {
-    render(): React.ReactNode {
-        return (
-            <button>
-                Button
-            </button>
-        )
-    }
+  render(): React.ReactNode {
+    return (
+      <button>
+        Button
+      </button>
+    )
+  }
 }
 
 export default Button;
---
+++ src/components/Form/index.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Button from "../Button";
+
+
+class Form extends React.Component {
+	render(): React.ReactNode {
+		return (
+			<form>
+				<div>
+					<label htmlFor="task">
+						Add a new study task
+					</label>
+					<input
+						id="task"
+						name="task"
+						type="text"
+						placeholder="What do you want to study?"
+						required
+					/>
+				</div>
+				<div>
+					<label htmlFor="time">
+						Time to study
+					</label>
+					<input
+						id="time"
+						type="time"
+						step="1"
+						name="time"
+						min="00:00:00"
+						max="01:30:00"
+					/>
+				</div>
+				<Button />
+			</form>
+		)
+	}
+}
+
+export default Form;
