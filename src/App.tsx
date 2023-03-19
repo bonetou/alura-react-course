c1ee8c3a398bea428885a789094857b22bded9e8
bonetou <h.boneto@outlook.com>
2023-03-19 17:23:03 -0300

    feat: creating the list component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import Button from './components/Button';
-import Form from './components/Form';
+import Form from './components/Form'
+import List from './components/List'
 
 
 function App() {
   return (
     <div className="App">
       <Form />
+      <List />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -37,4 +37,4 @@ class Form extends React.Component {
 	}
 }
 
-export default Form;
+export default Form
